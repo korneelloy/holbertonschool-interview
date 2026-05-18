5d6a484a11f55a9e7a78a6bcc41dd6bdc342f3f7
korneel <korneel.loy@gmail.com>
2026-05-18 12:05:58 +0200

    betty

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,8 +1,12 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
 /**
  * prototypes of functions
  */
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid(int grid1[3][3]);
 void checkgrid(int grid1[3][3]);
 void topplegrid(int grid1[3][3]);
+
+#endif /* SANDPILES_H */

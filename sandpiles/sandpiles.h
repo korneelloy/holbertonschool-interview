75f02bfcddb23948b397793f056175e240d9244b
korneel <korneel.loy@gmail.com>
2026-05-18 12:08:54 +0200

    betty

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -2,11 +2,22 @@
 #define SANDPILES_H
 
 /**
- * prototypes of functions
+ * sandpiles_sum - Computes the sum of two sandpiles
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
  */
-
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+/**
+ * checkgrid - Checks if grid is stable, topples it if not
+ * @grid1: 3x3 grid
+ */
 void checkgrid(int grid1[3][3]);
+
+/**
+ * topplegrid - Topples all unstable cells simultaneously
+ * @grid1: 3x3 grid
+ */
 void topplegrid(int grid1[3][3]);
 
 #endif /* SANDPILES_H */

ad3795771b7b7039488051e9fa7b761c598759c0
korneel <korneel.loy@gmail.com>
2026-06-05 13:59:38 +0200

    ex algo

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -45,7 +45,7 @@ def main():
         print_stats(total_size, status_counts)
         raise
 
-    if line_count % 10 != 0:
+    if line_count == 0 or line_count % 10 != 0:
         print_stats(total_size, status_counts)
 
 

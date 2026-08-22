017f8597fd54799abe560052e6ad9a024dc5ac2c
korneel <korneel.loy@gmail.com>
2026-08-22 16:11:41 +0200

    ex 0

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -49,7 +49,11 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	}
 
 	if (current_node != NULL && current_node->n == value)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+		       current_node->index, current_node->n);
 		return (current_node);
+	}
 
 	return (NULL);
 }

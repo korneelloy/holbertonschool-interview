701ad7613f9e8f8fc4cf6ca0a5ba183542994e44
korneel <korneel.loy@gmail.com>
2026-08-22 16:00:27 +0200

    ex 0

---
+++ linear_skip/0-linear_skip.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "search.h"
+
+/**
+ * linear_skip - search in skiplist
+ *
+ * @list: Pointer to the head of the list
+ * @value: the value we are lookign fro
+ *
+ * Return: node or null if not found
+ */
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *current_node;
+	skiplist_t *prev_express;
+	int current_value;
+
+	if (list == NULL)
+		return (NULL);
+
+	current_node = list;
+	prev_express = list;
+	current_value = list->n;
+
+	while (current_value < value && current_node->express != NULL)
+	{
+		prev_express = current_node;
+		current_node = current_node->express;
+		current_value = current_node->n;
+		printf("Value checked at index [%lu] = [%d]\n",
+		       current_node->index, current_value);
+	}
+
+	if (current_value > value)
+		current_node = prev_express;
+
+	while (current_node != NULL && current_node->n < value)
+	{
+		current_node = current_node->next;
+		if (current_node != NULL)
+			printf("Value checked at index [%lu] = [%d]\n",
+			       current_node->index, current_node->n);
+	}
+
+	if (current_node != NULL && current_node->n == value)
+		return (current_node);
+
+	return (NULL);
+}

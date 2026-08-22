1d3e4a0dccd9b839155aff9d9dc2a3e816ff16c7
korneel <korneel.loy@gmail.com>
2026-08-22 16:37:34 +0200

    ex 0

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -4,6 +4,34 @@
 #include "search.h"
 
 /**
+ * print_checked - print function
+ *
+ * @index: index
+ * @value: value
+ *
+ * Return: void
+ */
+
+void print_checked(long index, int value)
+{
+	printf("Value checked at index [%lu] = [%d]\n", index, value);
+}
+
+/**
+ * print_found - print function
+ *
+ * @index: index
+ * @index_two: index
+ *
+ * Return: void
+ */
+
+void print_found(long index, long index_two)
+{
+	printf("Value found between indexes [%lu] and [%lu]\n", index, index_two);
+}
+
+/**
  * linear_skip - search in skiplist
  *
  * @list: Pointer to the head of the list
@@ -14,46 +42,43 @@
 
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *current_node;
-	skiplist_t *prev_express;
+	skiplist_t *current_node, *prev_express, *tail;
 	int current_value;
 
 	if (list == NULL)
 		return (NULL);
-
 	current_node = list;
 	prev_express = list;
 	current_value = list->n;
-
 	while (current_value < value && current_node->express != NULL)
 	{
 		prev_express = current_node;
 		current_node = current_node->express;
 		current_value = current_node->n;
-		printf("Value checked at index [%lu] = [%d]\n",
-		       current_node->index, current_value);
+		print_checked(current_node->index, current_value);
 	}
-
-	printf("Value found between indexes [%lu] and [%lu]\n",
-	       prev_express->index, current_node->index);
-
 	if (current_value >= value)
+	{
+		print_found(prev_express->index, current_node->index);
 		current_node = prev_express;
-
+	}
+	else
+	{
+		tail = current_node;
+		while (tail->next != NULL)
+			tail = tail->next;
+		print_found(current_node->index, tail->index);
+	}
 	while (current_node != NULL && current_node->n < value)
 	{
 		if (current_node != NULL)
-			printf("Value checked at index [%lu] = [%d]\n",
-			       current_node->index, current_node->n);
+			print_checked(current_node->index, current_node->n);
 		current_node = current_node->next;
 	}
-
 	if (current_node != NULL && current_node->n == value)
 	{
-		printf("Value checked at index [%lu] = [%d]\n",
-		       current_node->index, current_node->n);
+		print_checked(current_node->index, current_node->n);
 		return (current_node);
 	}
-
 	return (NULL);
 }
